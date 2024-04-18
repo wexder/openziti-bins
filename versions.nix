bf3d60980dc2863346cc655de18814c8b3a8b579
Vladimir Zahradnik <wexder19@gmail.com>
2024-04-18 08:08:44 +0200

    migrate to v1

--- versions.nix
+++ versions.nix
@@ -9,32 +9,7 @@ in
 
       v1-0-0 = {
         version = "1.0.0";
-        hash = "sha256-C9wVfmjb8nE10Zlfa0MJduUy86L9CEIPMDsJx7MwAwk=";
-      };
-
-      v0-27-5 = {
-        version = "0.27.5";
-        hash = "sha256-C9wVfmjb8nE10Zlfa0MJduUy86L9CEIPMDsJx7MwAwk=";
-      };
-
-      v0-27-2 = {
-        version = "0.27.2";
-        hash = "sha256-3Fo2PoyibmT/pSALIN6gM4kSSv8kKgTzeNKa/vGI5gc=";
-      };
-
-      v0-26-10 = {
-        version = "0.26.10";
-        hash = "sha256-+16ufjL6ej4lGkRsA6wNBN4s8EeQc2utzEGBIJ0ijls=";
-      };
-
-      v0-26-9 = {
-        version = "0.26.9";
-        hash = "sha256-cp07b5MyzK6109l7lB11bBa2+sXzGwqC2QJExuSwL5k=";
-      };
-
-      v0-26-8 = {
-        version = "0.26.8";
-        hash = "sha256-wmHpL9TaytEnyFZ7FuDffUD4VwRUkQigJS++BiP1fZo=";
+        hash = "sha256-uUX5vHu/Ce0Iq/yefXu/ejYBI4VHtLli7bNglNAjJe0=";
       };
     };
 
@@ -43,32 +18,7 @@ in
 
       v1-0-0 = {
         version = "1.0.0";
-        hash = "sha256-C9wVfmjb8nE10Zlfa0MJduUy86L9CEIPMDsJx7MwAwk=";
-      };
-
-      v0-27-5 = {
-        version = "0.27.5";
-        hash = "sha256-bLOcK6Bh3J5dZ8+kudX/pxFaPVRP9GU0uHfO5xDBGiA=";
-      };
-
-      v0-27-2 = {
-        version = "0.27.2";
-        hash = "sha256-BsEsRuNEdMWrMTkokRHVrq9SjGsxw2aclrsVvxF595A=";
-      };
-
-      v0-26-10 = {
-        version = "0.26.10";
-        hash = "sha256-kIJak44wjWi6iLyKdiSifNhZSZmjyNrcLCx/ggVQArE=";
-      };
-
-      v0-26-9 = {
-        version = "0.26.9";
-        hash = "sha256-QA/ks618eI+yJH+sBJyygORq5bCLeVefq3m9xo11Pf4=";
-      };
-
-      v0-26-8 = {
-        version = "0.26.8";
-        hash = "sha256-OovvwJ6cwiktccqkPdTXy8IvS4EdYLrIxqnB8Dz2sWM=";
+        hash = "sha256-uUX5vHu/Ce0Iq/yefXu/ejYBI4VHtLli7bNglNAjJe0=";
       };
     };
 
@@ -79,51 +29,6 @@ in
         v1-0-0 = {
           version = "0.21.3";
           hash = "sha256-O9vA08WVEond+R447ZLyYBgDSaj2VHp+5C20FxxYWUc=";
-        };
-
-        v0-21-3 = {
-          version = "0.21.3";
-          hash = "sha256-O9vA08WVEond+R447ZLyYBgDSaj2VHp+5C20FxxYWUc=";
-        };
-
-        v0-20-22 = {
-          version = "0.20.22";
-          hash = "sha256-ji29IzPLrM5Qh1Tg+WTn5z222267aks7qVICQOQi22A=";
-        };
-
-        v0-20-21 = {
-          version = "0.20.21";
-          hash = "sha256-8/ci7ULITRcRpHnZcr7afcOt+y6aLfxeaRdJHN0Ma1U=";
-        };
-
-        v0-20-20 = {
-          version = "0.20.20";
-          hash = "sha256-dlFY+U7p1yfFhphlU1UZJek85K0QanYIi457U7dcGMM=";
-        };
-
-        v0-20-18 = {
-          version = "0.20.18";
-          hash = "sha256-D773ZeEs/NUp/lwVCKxYz5voq/MXeLiJU5YcB/Vcs8g=";
-        };
-
-        v0-20-6 = {
-          version = "0.20.6";
-          hash = "sha256-fyOJJ88DvRCVHNtlWt1eUJdH1XRAyeSgHeJTwxWM8e0=";
-        };
-
-        v0-20-2 = {
-          version = "0.20.2";
-          hash = "sha256-ZgeVSGqy12CQJEErzQ1gaXtJbv5bVncH66Li1X8D3P0=";
-        };
-
-        v0-20-0 = {
-          version = "0.20.0";
-          hash = "sha256-/AS8PUaBjfunEwXvWnVmwMQSdQ0CHYM+FpbCSploaeA=";
-        };
-
-        v0-19-11 = {
-          version = "0.19.11";
-          hash = "sha256-cZne4M7XZV+bpOq5moRexMqhKCkBQ8pMpa7A7oBOcX8=";
         };
       };
 
